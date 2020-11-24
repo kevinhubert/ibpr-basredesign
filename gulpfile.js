bec630cb3ae758cf0ca547ab1551efa57e571afb
Kevin Hubert <iamkevinhubert@gmail.com>
2020-11-24 12:07:02 -0600

    adding some changes

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("src/scss/*.scss", ['sass']);
     gulp.watch("src/scss/*/*.scss", ['sass']);
     gulp.watch("src/scss/*/*/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
